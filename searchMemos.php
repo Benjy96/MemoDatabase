088ae222ded1bdddcb0dbce0cbb1473cc7b8d264
Benjy96 <benjamindickey@hotmail.co.uk>
2017-10-14 20:53:04 +0100

    Display memo details on search bar

--- searchMemos.php
+++ searchMemos.php
@@ -15,11 +15,21 @@ if(strlen($query) > 0){
 					
 	for($i = 0; $i<($memos->length); $i++){
 		$currentMemo = $memos->item($i)->getAttribute("id");
+		$currentTitle = $memos->item($i)->childNodes->item(0)->nodeValue;
+		$currentSender = $memos->item($i)->parentNode->getAttribute("name");
 		if(stristr($currentMemo, $query)){
 			if($hint==""){
-				$hint = "<div class='panel-body' onclick='getCurrentById($currentMemo)'>" . $currentMemo . "</div>";
+				$hint = "<div class='panel-body' onclick='getCurrentById($currentMemo)'>(" . 
+				$currentMemo . ") " .
+				$currentTitle . 
+				", by ". $currentSender .
+				"</div>";
 			}else{
-				$hint = $hint . "<div class='panel-body' onclick='getCurrentById($currentMemo)'>" . $currentMemo . "</div>";
+				$hint = $hint . "<div class='panel-body' onclick='getCurrentById($currentMemo)'>(" . 
+				$currentMemo . ") " .
+				$currentTitle . 
+				", by " . $currentSender . 
+				"</div>";
 			}
 		}
 	}
